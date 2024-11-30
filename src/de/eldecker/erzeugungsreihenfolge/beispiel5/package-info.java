cc8092767872d284aea360577563c0f653de010e
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-11-30 15:42:11 +0100

    beispiel5/package-info.java: Konsole-Output dazu

--- src/de/eldecker/erzeugungsreihenfolge/beispiel5/package-info.java
+++ src/de/eldecker/erzeugungsreihenfolge/beispiel5/package-info.java
@@ -1,5 +1,20 @@
 /**
  * Die Klasse {@link Unterklasse} greift im Konstruktor auf alle statischen 
  * und nicht-statischen Klasse aus der Oberklasse und ihr selbst zu.
+ * 
+ * Ausgabe des Programms:
+ * <pre>
+ * O1
+ * O4
+ * O2
+ * O3
+ * Oberklasse
+ * O5
+ * 
+ * o1=de.eldecker.erzeugungsreihenfolge.beispiel5.O1@1f32e575
+ * o2=de.eldecker.erzeugungsreihenfolge.beispiel5.O2@279f2327
+ * o3=de.eldecker.erzeugungsreihenfolge.beispiel5.O3@2ff4acd0
+ * o4=de.eldecker.erzeugungsreihenfolge.beispiel5.O4@54bedef2
+ * o5=de.eldecker.erzeugungsreihenfolge.beispiel5.O5@5caf905d
  */
 package de.eldecker.erzeugungsreihenfolge.beispiel5;
