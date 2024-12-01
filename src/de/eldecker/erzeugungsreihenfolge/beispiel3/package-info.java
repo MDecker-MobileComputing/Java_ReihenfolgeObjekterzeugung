b274c2d8378a933683ae844d1a1326e645a14379
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-12-01 21:26:37 +0100

    beispiel3/package-info.java erweitert

--- src/de/eldecker/erzeugungsreihenfolge/beispiel3/package-info.java
+++ src/de/eldecker/erzeugungsreihenfolge/beispiel3/package-info.java
@@ -7,5 +7,16 @@
  * O2
  * O4
  * </pre>
+ * <br>
+ * 
+ * An diesem Beispiel sieht man, dass zuerst die Objektvariablen vor dem
+ * "Haupt-Teil" des Konstruktors abgearbeitet werden ({@code O1} wird
+ * vor {@code O3} ausgegeben, {@code O2} vor {@code O4}).
+ * <br>
+ * 
+ * Außerdem sieht man, dass zuerst die Objektvariablen der Oberklasse
+ * initialisiert werden, dann der "Haupt-Teil" des Konstruktors der
+ * Oberklasse, bevor dann die Objektvariablen der Unterklasse initialisiert
+ * werden.
  */
 package de.eldecker.erzeugungsreihenfolge.beispiel3;
